aacaedb1d25038e3f65766994fd82ea2044f00d0
Cyber_pineapple <cyber_pineapple@mail.ru>
2019-06-04 12:02:41 +0300

    add new functionality

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const input = document.querySelector("[data-js-id=input]");
 const main = document.querySelector("[data-js-id=main]");
-const buttonAll = document.querySelector("[data-js-id=buttonAll]");
-const buttonCompleted = document.querySelector("[data-js-id=buttonCompleted]");
-const buttonActive = document.querySelector("[data-js-id=buttonActive]");
 const buttonCompleteAll = document.querySelector("[data-js-id=completedAll]");
+const radioButton = document.getElementsByName('button');
 
-let list = [];
+let listAll = [];
+let listActive = [];
+let listCompleted
 input.addEventListener("keypress", (event) => add(event));
 window.addEventListener("DOMContentLoaded", () => load());
 main.children[0].addEventListener('change', () => toggleAll());
+radioButton[0].addEventListener('change', () => view());
+radioButton[1].addEventListener('change', () => view());
+radioButton[2].addEventListener('change', () => view());
+
+
 
 
 add = () => {
@@ -19,81 +24,28 @@ add = () => {
 
 del = el => {
   main.children[1].removeChild(el);
-  list = list.filter(value => {
-    return value.id !== el.getAttribute("data-list-id");
+  console.log(listAll);
+  listAll = listAll.filter(value => {
+    console.log('value.id ', value.id, ' atribute', el.getAttribute("data-list-id"));
+    return value.id != el.getAttribute("data-list-id");
   });
-  localStorage.setItem("list", JSON.stringify(list));
+  localStorage.setItem("list", JSON.stringify(listAll));
+  console.log(listAll);
 };
 
 load = () => {
-  setElements();
+  view();
 };
 
 createNewElement = () => {
-  const el = document.createElement("li");
-  const checkbox = document.createElement("input");
-  const id = Date.now();
-  const label = document.createElement("label");
-  const deleteButton = document.createElement('div');
-
-  deleteButton.classList.add('delete-button');
-  deleteButton.addEventListener('click', () => del(el));
-  checkbox.type = "checkbox";
-  checkbox.classList.add("item-checkbox");
-  label.classList.add("label");
-  el.appendChild(checkbox);
-  el.appendChild(label);
-  el.appendChild(deleteButton);
-  el.addEventListener('mouseover', () => viewDeleteButton(el))
-  el.addEventListener('mouseout', () => hideDeleteButton(el))
-  checkbox.addEventListener("change", () => toggleCheckBox(el));
-  label.innerText = input.value;
-  el.children[1].addEventListener("dblclick", () => editElement(el));
-  el.classList.add("message");
-  el.dataset.listId = id;
-  main.children[1].appendChild(el);
-  input.value = "";
-  list.push({
-    text: el.innerText,
+  listAll.push({
+    text: input.value,
     completed: false,
-    id: el.getAttribute('data-list-id')
-
+    id: Date.now()
   });
-  localStorage.setItem('list', JSON.stringify(list));
-};
-
-setElements = () => {
-  let elements = JSON.parse(localStorage.getItem("list"));
-  for (key in elements) {
-    const el = document.createElement("li");
-    const checkbox = document.createElement("input");
-    const id = Date.now();
-    const label = document.createElement("label");
-    const deleteButton = document.createElement('div');
-
-    deleteButton.classList.add('delete-button');
-    deleteButton.addEventListener('click', () => del(el));
-    checkbox.type = "checkbox";
-    checkbox.classList.add("item-checkbox");
-    label.classList.add("label");
-    el.appendChild(checkbox);
-    el.appendChild(label);
-    el.appendChild(deleteButton);
-    el.addEventListener('mouseover', () => viewDeleteButton(el))
-    el.addEventListener('mouseout', () => hideDeleteButton(el))
-    checkbox.addEventListener("change", () => toggleCheckBox(el));
-    label.innerText = elements[key].text;
-    el.children[1].addEventListener("dblclick", () => editElement(el));
-    el.classList.add("message");
-    el.dataset.listId = elements[key].id;
-    main.children[1].appendChild(el);
-
-    list.push({
-      text: elements[key].text,
-      id: elements[key].id,
-      completed: elements[key].completed
-    });
-  }
+  input.value = "";
+  localStorage.setItem('list', JSON.stringify(listAll));
+  view();
 };
 
 editElement = el => {
@@ -116,17 +68,37 @@ addNewValueToMessage = (el, input) =>{
   if (input.value){
     el.children[1].innerText = input.value;
     el.removeChild(input);
+    for (key in listAll){
+      if (listAll[key].id == el.getAttribute("data-list-id")){
+        listAll[key].text = el.children[1].innerText;
+        localStorage.setItem('list', JSON.stringify(listAll));
+      }
+    }
   } else {
-    el.parentNode.removeChild(el);
+    del(el);
   }
 }
 
 toggleCheckBox = el => {
+  console.log(listAll);
   if (el.children[0].checked) {
-    el.children[1].classList.add('completed');
+    for (key in listAll){
+      if (listAll[key].id == el.getAttribute("data-list-id")){
+        listAll[key].completed = true;
+        console.log(listAll[key]);
+      }
+    }
   } else {
-    el.children[1].classList.remove('completed');
+    for (key in listAll){
+      if (listAll[key].id == el.getAttribute("data-list-id")){
+        listAll[key].completed = false;
+        console.log(listAll[key]);
+      }
+    }
   }
+  console.log(listAll);
+  localStorage.setItem('list', JSON.stringify(listAll));
+  view();
 };
 
 viewDeleteButton = (el) => {
@@ -138,17 +110,145 @@ hideDeleteButton = (el) => {
 }
 
 toggleAll = () => {
-  let list = main.children[1];
-  console.log(list);
+  let elements = main.children[1];
+  console.log(listAll);
   if (main.children[0].checked){
-    for (key in list.children){
-      list.children[key].children[0].checked = true;
-      list.children[key].children[1].classList.add('completed');
+    for (key in elements.children){
+      elements.children[key].children[0].checked = true;
+      elements.children[key].children[1].classList.add('completed');
+      listAll[key].completed = true;
+      localStorage.setItem('list', JSON.stringify(listAll));
     }
+    view();
   } else {
-    for (key in list.children){
-      list.children[key].children[0].checked = false;
-      list.children[key].children[1].classList.remove('completed');
+    for (key in elements.children){
+      elements.children[key].children[0].checked = false;
+      elements.children[key].children[1].classList.remove('completed');
+      listAll[key].completed = false;
+      localStorage.setItem('list', JSON.stringify(listAll));
     }
   }
+  view();
 }
+
+view = () =>{
+  console.log(listAll);
+  if (radioButton[0].checked){
+    while (main.children[1].firstChild){
+      main.children[1].removeChild(main.children[1].firstChild);
+    }
+    listAll = [];
+    setElements();
+  } else if (radioButton[1].checked){
+    while (main.children[1].firstChild){
+      main.children[1].removeChild(main.children[1].firstChild);
+    }
+    setCompletedElements();
+  } else if (radioButton[2].checked){
+    while (main.children[1].firstChild){
+      main.children[1].removeChild(main.children[1].firstChild);
+    }
+    setActiveElements();
+  }
+}
+
+setElements = () => {
+  let elements = JSON.parse(localStorage.getItem("list"));
+  for (key in elements) {
+    const el = document.createElement("li");
+    const checkbox = document.createElement("input");
+    const label = document.createElement("label");
+    const deleteButton = document.createElement('div');
+
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', () => del(el));
+    checkbox.type = "checkbox";
+    checkbox.classList.add("item-checkbox");
+    label.classList.add("label");
+    el.appendChild(checkbox);
+    el.appendChild(label);
+    el.appendChild(deleteButton);
+    el.addEventListener('mouseover', () => viewDeleteButton(el))
+    el.addEventListener('mouseout', () => hideDeleteButton(el))
+    checkbox.addEventListener("change", () => toggleCheckBox(el));
+    checkbox.checked = elements[key].completed;
+    if (checkbox.checked){
+      el.children[1].classList.add('completed');
+    }
+    label.innerText = elements[key].text;
+    el.children[1].addEventListener("dblclick", () => editElement(el));
+    el.classList.add("message");
+    el.dataset.listId = elements[key].id;
+    main.children[1].appendChild(el);
+
+    listAll.push({
+      text: elements[key].text,
+      id: elements[key].id,
+      completed: elements[key].completed
+    });
+  }
+};
+
+setCompletedElements = () => {
+  let elements = JSON.parse(localStorage.getItem("list"));
+  elements = elements.filter( (value) => {
+    return value.completed === true}
+  );
+  for (key in elements) {
+    const el = document.createElement("li");
+    const checkbox = document.createElement("input");
+    const label = document.createElement("label");
+    const deleteButton = document.createElement('div');
+
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', () => del(el));
+    checkbox.type = "checkbox";
+    checkbox.checked = true;
+    checkbox.classList.add("item-checkbox");
+    label.classList.add("label");
+    label.classList.add('completed');
+    label.classList.add("checked");
+    el.appendChild(checkbox);
+    el.appendChild(label);
+    el.appendChild(deleteButton);
+    el.addEventListener('mouseover', () => viewDeleteButton(el))
+    el.addEventListener('mouseout', () => hideDeleteButton(el))
+    checkbox.addEventListener("change", () => toggleCheckBox(el));
+    label.innerText = elements[key].text;
+    el.children[1].addEventListener("dblclick", () => editElement(el));
+    el.classList.add("message");
+    el.dataset.listId = elements[key].id;
+    main.children[1].appendChild(el);
+  }
+};
+
+setActiveElements = () => {
+  let elements = JSON.parse(localStorage.getItem("list"));
+  elements = elements.filter( (value) => {
+    return value.completed === false}
+  );
+  for (key in elements) {
+    const el = document.createElement("li");
+    const checkbox = document.createElement("input");
+    const label = document.createElement("label");
+    const deleteButton = document.createElement('div');
+
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', () => del(el));
+    checkbox.type = "checkbox";
+    checkbox.checked = false;
+    checkbox.classList.add("item-checkbox");
+    label.classList.add("label");
+    el.appendChild(checkbox);
+    el.appendChild(label);
+    el.appendChild(deleteButton);
+    el.addEventListener('mouseover', () => viewDeleteButton(el))
+    el.addEventListener('mouseout', () => hideDeleteButton(el))
+    checkbox.addEventListener("change", () => toggleCheckBox(el));
+    label.innerText = elements[key].text;
+    el.children[1].addEventListener("dblclick", () => editElement(el));
+    el.classList.add("message");
+    el.dataset.listId = elements[key].id;
+    main.children[1].appendChild(el);
+  }
+};
